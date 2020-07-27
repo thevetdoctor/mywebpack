82641fe4d02594b8884c52b2e6dd147133c2d5ff
obafemi oderanti <thevetdoctor.gmail.com>
2020-07-27 11:30:12 +0100

    tweaking css

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,7 @@ const $ = require("jquery");
 
 let peopleList = people(); 
 $.each(peopleList, (x) => {
-    $("body").append("<button class='rounded bg-green-200 mx-2 my-3 px-3 py-3'>" + peopleList[x].name + "</button>")
-    // $("body").append("<p></p>")
+    $("body").append(`<button class='rounded bg-green-400 mx-2 my-3 px-3 py-3 border border-white-1 hover:bg-green 600 hover:text-white'>${peopleList[x].name}</button>`);
 }); 
    
 console.log(peopleList);
